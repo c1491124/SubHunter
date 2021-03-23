b652e4f0c2f8b28a09677b17d5e9db3103ef1ef5
up149 <upload1491124@gmail.com>
2021-03-23 14:27:58 +0800

    chap3 finished.

--- app/src/main/java/com/example/subhunter/SubHunter.java
+++ app/src/main/java/com/example/subhunter/SubHunter.java
@@ -2,13 +2,14 @@ package com.example.subhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 
-public class SubHunter extends AppCompatActivity {
+public class SubHunter extends Activity {
     // These variables can be "seen"
     // throughout the SubHunter class
     int numberHorizontalPixels;
@@ -40,8 +41,12 @@ public class SubHunter extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-
-
+        // Initialize our size based variables
+        // based on the screen resolution
+        numberHorizontalPixels = size.x;
+        numberVerticalPixels = size.y;
+        blockSize = numberHorizontalPixels / gridWidth;
+        gridHeight = numberVerticalPixels / blockSize;
         Log.d("Debugging", "In onCreate");
         newGame();
         draw();
@@ -53,13 +58,20 @@ public class SubHunter extends AppCompatActivity {
         happen when the app is first started
         and after the player wins a game.
      */
-    void newGame(){Log.d("Debugging", "In newGame");}
+    void newGame() {
+        Log.d("Debugging", "In newGame");
+    }
+
     /*
         Here we will do all the drawing.
         The grid lines, the HUD and
         the touch indicator
      */
-    void draw(){Log.d("Debugging", "In draw");}
+    void draw() {
+        Log.d("Debugging", "In draw");
+        printDebuggingText();
+
+    }
 
     /*
         This part of the code will
@@ -67,26 +79,54 @@ public class SubHunter extends AppCompatActivity {
         has tapped the screen
      */
     @Override
-    public boolean onTouchEvent(MotionEvent motionEvent){
+    public boolean onTouchEvent(MotionEvent motionEvent) {
         Log.d("Debugging", "In onTouchEvent");
         takeShot();
         return true;
     }
+
     /*
         The code here will execute when
         the player taps the screen. It will
         calculate the distance from the sub'
         and decide a hit or miss
      */
-    void takeShot(){ Log.d("Debugging", "In takeShot");
-        draw();}
+    void takeShot() {
+        Log.d("Debugging", "In takeShot");
+        draw();
+    }
+
     // This code says "BOOM!"
-    void boom(){
+    void boom() {
 
     }
-    // This code prints the debugging text
-    void printDebuggingText(){
 
+    // This code prints the debugging text
+    void printDebuggingText() {
+        Log.d("numberHorizontalPixels",
+                "" + numberHorizontalPixels);
+        Log.d("numberVerticalPixels",
+                "" + numberVerticalPixels);
+
+        Log.d("blockSize", "" + blockSize);
+        Log.d("gridWidth", "" + gridWidth);
+        Log.d("gridHeight", "" + gridHeight);
+
+        Log.d("horizontalTouched",
+                "" + horizontalTouched);
+        Log.d("verticalTouched",
+                "" + verticalTouched);
+        Log.d("subHorizontalPosition",
+                "" + subHorizontalPosition);
+        Log.d("subVerticalPosition",
+                "" + subVerticalPosition);
+
+        Log.d("hit", "" + hit);
+        Log.d("shotsTaken", "" + shotsTaken);
+        Log.d("debugging", "" + debugging);
+
+        Log.d("distanceFromSub",
+                "" + distanceFromSub);
     }
 
 
